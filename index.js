ef620c4c1f9ed0f7369c88b2972769ee75ee5f2f
Kris Janssen Cabansag <kjs.cabansag@gmail.com>
2024-02-20 21:03:37 -0500

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+multiply = num1 * num2;
+
+const random = 1;
+function number(random){
+   random.isInteger();
+}
+
+const num3 = 16;
+const num4 = 4;
+
+function mod(num3, num4){
+    return num3 / num4;
+}
+mod = num3 / num4;
+
+
+const max = Math.max(5, 10, 15, 20);
